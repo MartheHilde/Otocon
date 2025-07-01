e7bc8eb782c17dbbd7e3336432809817be437e4d
Marthe Hilde <marunnie2@gmail.com>
2025-07-01 20:18:51 +0200

    Snippets

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function toggleMenu() {
   const nav = document.getElementById("navLinks");
   nav.classList.toggle("show");
 }
+// HEADER SNIPPET
+fetch('snippets/header.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('main-header').innerHTML = data;
+  });
