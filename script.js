28f0d0e33c7c8a5de696e6c9a7f80d8effc7cc12
Marthe Hilde <marunnie2@gmail.com>
2025-07-05 20:36:32 +0200

    Mass styling update og litt innhold

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ fetch('snippets/header.html')
   .then(data => {
     document.getElementById('main-header').innerHTML = data;
   });
+
+  // FOOTER SNIPPET
+fetch('snippets/footer.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('main-footer').innerHTML = data;
+  });
